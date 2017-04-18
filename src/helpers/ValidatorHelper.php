c5273c191a5938146c7fb8cfe9e5e07d6f44ed68
Rafael Piazdecki <rafael@piazdecki.pl>
2017-04-18 06:30:13 +0200

    extended Validator so there can be more errors with error codes now

--- src/helpers/ValidatorHelper.php
+++ src/helpers/ValidatorHelper.php
@@ -19,17 +19,31 @@ class ValidatorHelper
 
     public function isNumber($field, $value) : bool
     {
-        return is_numeric((string)$value);
+        if (!is_numeric((string)$value)) {
+            $this->setError("$field-000", "$field is not a number");
+            return false;
+        }
+
+        return true;
     }
 
     public function required($field, $value) : bool
     {
-        return (strlen((string)$value) > 0);
+        if (strlen((string)$value) == 0) {
+            $this->setError("$field-001", "$field is required");
+            return false;
+        }
+        return true;
     }
 
     public function isCVV2LengthValid($field, $value) : bool
     {
-        return (strlen((string)$value) == 3);
+        if (strlen((string)$value) != 3) {
+            $this->setError("$field-002", "$field is not valid. Valid format: 3-digit number");
+            return false;
+        }
+
+        return true;
     }
 
     public function isCreditCardNumberValid($field, $value) : bool
@@ -51,7 +65,12 @@ class ValidatorHelper
             $total += $digit;
         }
 
-        return ($total % 10 == 0);
+        if ($total % 10 != 0) {
+            $this->setError("$field-003", "Credit card number is not valid.");
+            return false;
+        }
+
+        return true;
     }
 
     public function isExpirationDateValid($field, $value) : bool
@@ -60,16 +79,20 @@ class ValidatorHelper
 
         if (count($date) != 2 || strlen($value) != 7)
         {
+            $this->setError("$field-004", "$field is not valid. Valid format MM-YYYY ex. 01-2017");
             return false;
         }
         elseif ((int)$date[0] > 12 || (int)$date[0] < 1)
         {
+            $this->setError("$field-005", "$field is not valid. Valid format MM-YYYY where MM is between 01 and 12");
             return false;
         }
 
         $expires = \DateTime::createFromFormat('mY', $date[0].$date[1]);
         $now     = new \DateTime();
+
         if ($expires < $now) {
+            $this->setError("$field-006", "expired or $field is invalid");
             return false;
         }
 
@@ -78,18 +101,35 @@ class ValidatorHelper
 
     public function isEmailValid($field, $value) : bool
     {
-        return filter_var($value, FILTER_VALIDATE_EMAIL);
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            $this->setError("$field-007", "$field is not valid. Please check $field format");
+            return false;
+        }
+
+        return true;
     }
 
     public function isPaymentMethodValid($field, $value) : bool
     {
-        return in_array($value, self::VALID_PAYMENT_METHODS) ? true : false;
+        if (!in_array($value, self::VALID_PAYMENT_METHODS)) {
+            $this->setError("$field-008", "$field is not valid payment method. Valid methods: mobile or creditCard");
+            return false;
+        }
+        return true;
     }
 
     public function isPhoneNumberValid($field, $value) : bool
     {
-        return (bool)preg_match('/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \/]?)?((?:\(?\d{1,}\)?[\-\.\ \/]?)' .
-            '{0,})(?:[\-\.\ \/]?(?:#|ext\.?|extension|x)[\-\.\ \/]?(\d+))?$/i', $value);
+        if (!(bool)preg_match('/(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\\\/]?)?((?:\(?\d{1,}\)?[\-\.' .
+                              '\ \\\\\/]?){0,})(?:[\-\.\ \\\\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\\\/]?(\d+))?/i', $value)) {
+            $this->setError("$field-009", "$field is not valid. Possible formats: (+351) 282 43 50 50, 90191919908, 555-8909, " .
+                                           "001 6867684, 001 6867684x1, 1 (234) 567-8901, 1-234-567-8901 x1234, " .
+                                           "1-234-567-8901 ext1234, 1-234 567.89/01 ext.1234, 1(234)5678901x1234, (123)8575973, " .
+                                           "(0055)(123)8575973");
+            return false;
+        }
+
+        return true;
     }
 
     public function getErrors() : array
@@ -97,9 +137,9 @@ class ValidatorHelper
         return $this->errors;
     }
 
-    public function setError($field, $message) : ValidatorHelper
+    public function setError($code, $message) : ValidatorHelper
     {
-        $this->errors[$field] = $message;
+        $this->errors[$code] = $message;
 
         return $this;
     }
@@ -109,10 +149,7 @@ class ValidatorHelper
         foreach ($fields as $field => $rules)
         {
             foreach ($rules as $rule) {
-                if (!$this->$rule($field, $data[$field] ?? ($data->$field ?? '')))
-                {
-                    $this->setError($field, "$field is not valid");
-                }
+                $this->$rule($field, $data[$field] ?? '');
             }
         }
     }
